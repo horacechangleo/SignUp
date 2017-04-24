32b6bfd557ac9ac7d3c0cf1f25602bcd3f957e11
horacechangleo <horacechangleo@gmail.com>
2017-04-24 20:39:44 +0800

    修正－位置

--- js/action.js
+++ js/action.js
@@ -9,11 +9,10 @@ function hid() {
 $(document).ready(function() {
   $("button[type=submit]").click(()=> {
     $.ajax({
-      url:'do.njs',
+      url:'js/do.njs',
       method: 'GET',
       data:{mail:$('input[name=mail]').val(),user:$('input[name=user]').val(),pw:$('input[name=pw]').val()},
       error: function(err){
-        console.log(err);
         $("button").html("error");
       },
 
